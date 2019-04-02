02da5832b9a4b5c7cdaa798ea89951ba1e3444a7
Alexey Shmalko <rasen.dubi@gmail.com>
2019-04-02 20:12:23 +0300

    [rust2] Restructure to match architecture in the guide

--- rust2/src/readline.rs
+++ rust2/src/readline.rs
@@ -1,11 +1,22 @@
 use rustyline::Editor;
 
-pub fn read(prompt: &str) -> rustyline::Result<String> {
-    let mut rl = Editor::<()>::new();
-    let _ = rl.load_history("mal_history.txt");
+pub struct Reader {
+    rl: Editor<()>,
+}
 
-    let input = rl.readline(prompt)?;
-    rl.add_history_entry(input.as_ref());
-    rl.save_history("mal_history.txt")?;
-    Ok(input)
+impl Reader {
+    pub fn new(history_file: &str) -> Reader {
+        let mut result = Reader {
+            rl: Editor::<()>::new(),
+        };
+        let _ = result.rl.load_history(history_file);
+        result
+    }
+
+    pub fn readline(&mut self, prompt: &str) -> rustyline::Result<String> {
+        let input = self.rl.readline(prompt)?;
+        self.rl.add_history_entry(input.as_ref());
+        self.rl.save_history("mal_history.txt")?;
+        Ok(input)
+    }
 }
--- rust2/src/step0_repl.rs
+++ rust2/src/step0_repl.rs
@@ -5,31 +5,34 @@ mod readline;
 use rustyline::error::ReadlineError;
 
 const PROMPT: &str = "user> ";
+const HISTORY_FILE: &str = "mal_history.txt";
 
 fn main() {
+    let mut editor = readline::Reader::new(HISTORY_FILE);
     loop {
-        match rep() {
-            Ok(_) => (),
+        match editor.readline(PROMPT) {
+            Ok(line) => println!("{}", rep(&line)),
             Err(ReadlineError::Eof) | Err(ReadlineError::Interrupted) => return,
             Err(error) => {
                 println!("Error: {}", error);
+                return;
             },
         }
     }
 }
 
-fn read() -> rustyline::Result<String> {
-    readline::read(PROMPT)
+fn read(str: &String) -> &String {
+    str
 }
 
 fn eval(str: &String) -> &String {
     str
 }
 
-fn print(str: &String) {
-    println!("{}", str);
+fn print(str: &String) -> &String {
+    str
 }
 
-fn rep() -> rustyline::Result<()> {
-    Ok(print(eval(&read()?)))
+fn rep(str: &String) -> &String {
+    print(eval(read(str)))
 }
